b794d2496db8aea122aa0db59bda5965795b8011
yangsen <15763942407@163.com>
2018-10-31 12:23:28 +0800

    doubleMaxArea

--- src/com/zeyi/arithmetic/ContainerWithMostWater.java
+++ src/com/zeyi/arithmetic/ContainerWithMostWater.java
@@ -4,8 +4,14 @@ public class ContainerWithMostWater {
     public static void main(String agrs[]){
         int [] hei = new int[]{1,8,6,2,5,4,8,3,7};
         System.out.println(maxArea(hei));
+        System.out.println(doubleMaxArea(hei));
     }
 
+    /**
+     *  暴力法
+     * @param height
+     * @return
+     */
     public static int maxArea(int[] height) {
         int re = 0;
         for (int i=0;i<height.length;i++){
@@ -16,9 +22,23 @@ public class ContainerWithMostWater {
                 }
             }
         }
-
-
-
         return re;
+    }
+
+    /**
+     * 双指针法
+     * @param height
+     * @return
+     */
+    public static int doubleMaxArea(int[] height){
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
     }
 }
